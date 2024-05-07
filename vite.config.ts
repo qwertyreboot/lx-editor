6a8fc49ce3e6916c60fbced47fbb3e087f8b9e5f
khavinshankar <khavinshankar@gmail.com>
2024-05-07 11:22:29 +0530

    fix styling issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   build: {
@@ -23,5 +24,12 @@ export default defineConfig({
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({
+      insertTypesEntry: true,
+      include: ["src/**/*", "index.ts"],
+    }),
+  ],
 });
