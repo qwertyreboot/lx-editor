a366ae051c642e3865cb5e321b46b5c25ae7a53a
khavinshankar <khavinshankar@gmail.com>
2024-05-10 09:18:19 +0530

    removed injected styles

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 import dts from "vite-plugin-dts";
-import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   build: {
@@ -26,7 +25,6 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    libInjectCss(),
     dts({
       insertTypesEntry: true,
       include: ["src/**/*", "index.ts"],
